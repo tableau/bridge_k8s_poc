e8396f5cc9ec2a40fc2d6c957d0e1e60ce6a6968
Gustavo Armenta <garmenta@tableau.com>
2023-01-20 15:02:04 -0600

    rename variable

--- codepipeline/codepipeline.sh
+++ codepipeline/codepipeline.sh
@@ -8,13 +8,12 @@ cp template/codepipeline.yaml .
 sed -i.bak "s|\$AWS_ACCOUNT_ID|$AWS_ACCOUNT_ID|" codepipeline.yaml
 sed -i.bak "s|\$AWS_ORGANIZATION_ID|$AWS_ORGANIZATION_ID|" codepipeline.yaml
 
-# run this section for each service name (e.g. tableau_bridge, pat_client)
-service_name=tableau_bridge
-aws_service_name=$(sed -r 's/_/-/g' <<< "$service_name")
+# run for each repository name (e.g. tableau_bridge, pat_client)
+repository_name=tableau_bridge
 aws cloudformation create-stack \
-    --stack-name "codepipeline-$aws_service_name" \
+    --stack-name "$(echo "codepipeline-$repository_name" | sed -r 's/_/-/g')" \
     --template-body file://codepipeline.yaml \
-    --parameters ParameterKey=RepositoryName,ParameterValue=$service_name \
+    --parameters ParameterKey=RepositoryName,ParameterValue=$repository_name \
                  ParameterKey=SecurityGroupIds,ParameterValue=sg-0cb844360303ffabd \
                  ParameterKey=Subnets,ParameterValue=subnet-067a0adb670ee49c8 \
                  ParameterKey=VpcId,ParameterValue=vpc-0cef6ccc5c7c8318e \
