45a0ec167170728d2a578f1d35e29b782d934d91
Gustavo Armenta <garmenta@tableau.com>
2023-01-23 11:04:37 -0600

    codepipeline.sh requires x

--- codepipeline/codepipeline.sh
+++ codepipeline/codepipeline.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit; set -o nounset; set -o pipefail; set -o xtrace;
 
+: "${REPOSITORY_NAME:?REPOSITORY_NAME is required}"
+
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text --profile saml)
 AWS_ORGANIZATION_ID=$(aws organizations describe-organization --query Organization.Id --output text --profile saml)
 
@@ -8,18 +10,13 @@ cp template/codepipeline.yaml .
 sed -i.bak "s|\$AWS_ACCOUNT_ID|$AWS_ACCOUNT_ID|" codepipeline.yaml
 sed -i.bak "s|\$AWS_ORGANIZATION_ID|$AWS_ORGANIZATION_ID|" codepipeline.yaml
 
-# run for each repository name (e.g. tableau_bridge, pat_client)
-repository_name=tableau_bridge
 aws cloudformation create-stack \
-    --stack-name "$(echo "codepipeline-$repository_name" | sed -r 's/_/-/g')" \
+    --stack-name "$(echo "codepipeline-$REPOSITORY_NAME" | sed -r 's/_/-/g')" \
     --template-body file://codepipeline.yaml \
-    --parameters ParameterKey=RepositoryName,ParameterValue=$repository_name \
+    --parameters ParameterKey=RepositoryName,ParameterValue="$REPOSITORY_NAME" \
+                 ParameterKey=S3Key,ParameterValue="codepipeline/repositories/$REPOSITORY_NAME" \
                  ParameterKey=SecurityGroupIds,ParameterValue=sg-0cb844360303ffabd \
                  ParameterKey=Subnets,ParameterValue=subnet-067a0adb670ee49c8 \
                  ParameterKey=VpcId,ParameterValue=vpc-0cef6ccc5c7c8318e \
     --capabilities CAPABILITY_NAMED_IAM \
     --profile saml
-
-# delete generated files
-rm codepipeline.yaml
-rm codepipeline.yaml.bak
