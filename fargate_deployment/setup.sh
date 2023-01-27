ae60dcbd5584478931c81bf73240fcbd224465b5
Gustavo Armenta <garmenta@tableau.com>
2023-01-27 09:05:17 -0600

    add argument secret name

--- fargate_deployment/setup.sh
+++ fargate_deployment/setup.sh
@@ -1,21 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -o errexit; set -o nounset; set -o pipefail; set -o xtrace;
 
 function help(){
-    echo "USAGE: setup.sh -o <create or delete> -v <vpc name> -c <cluster name> -r <AWS region>"
+    echo "USAGE: setup.sh -o <create or delete> -v <vpc name> -s <secret name> -c <cluster name> -r <AWS region>"
 }
 
 unset -v Operation
-unset -v vpc_name
+unset -v VPC_NAME
+unset -v SECRET_ID
 unset -v CLUSTER
 unset -v REGION
 
 PASSED_ARGS=$@
 if [[ ${#PASSED_ARGS} -ne 0 ]]
 then
-    while getopts ":o:v:c:r:h" opt; do
+    while getopts ":o:v:s:c:r:h" opt; do
     case "$opt" in
         o) Operation=$OPTARG ;;
-        v) vpc_name=$OPTARG ;;
+        v) VPC_NAME=$OPTARG ;;
+        s) SECRET_ID=$OPTARG ;;
         c) CLUSTER=$OPTARG ;;
         r) REGION=$OPTARG ;;
         h) help ;;
@@ -29,26 +32,13 @@ else
 fi
 shift "$((OPTIND-1))"
 
-if [ -z "$Operation" ] || [ -z "$vpc_name" ] || [ -z "$CLUSTER" ] || [ -z "$REGION" ]; then
+if [ -z "$Operation" ] || [ -z "$VPC_NAME" ] || [ -z "$CLUSTER" ] || [ -z "$REGION" ]; then
         echo 'Missing -h or -u or -c or -r' >&2
         help
         exit 1
 fi
 
-SECRETARN=$(aws secretsmanager get-secret-value --secret-id SiteOneSecret --query ARN  --output text --profile saml)
-
-cp bridge-depolyment-policy.json bridge-depolyment-policy_new.json
-sed -i.bak -e "s|\$SECRETARN|$SECRETARN|" "bridge-depolyment-policy_new.json"
-
-BRIDGE_DEPLOYMENT_POLICY_ARN=$(aws iam list-policies --query 'Policies[?PolicyName==`bridge-deployment-policy`].Arn' --output text  --profile saml)
-if [[ $BRIDGE_DEPLOYMENT_POLICY_ARN != arn* ]]; then
-    BRIDGE_DEPLOYMENT_POLICY_ARN=$(aws --query Policy.Arn \
-            --output text iam create-policy \
-            --policy-name bridge-deployment-policy \
-            --policy-document file://"bridge-depolyment-policy_new.json" --profile saml)
-fi
-
-echo BRIDGE_DEPLOYMENT_POLICY_ARN is $BRIDGE_DEPLOYMENT_POLICY_ARN
+SECRET_ARN=$(aws secretsmanager get-secret-value --secret-id "$SECRET_ID" --query ARN --output text --profile saml)
 
 # if service_role doesn't exists, create using steps at https://docs.aws.amazon.com/eks/latest/userguide/service_IAM_role.html#create-service-role
 service_role_arn=$(aws iam list-roles --query 'Roles[?RoleName==`eksClusterRole`].Arn' --output text  --profile saml)
@@ -64,7 +54,7 @@ if [[ $service_role_arn != arn* ]]; then
 
 fi
 
-echo service_role_arn is $service_role_arn
+echo "service_role_arn is $service_role_arn"
 
 ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text --profile saml)
 cp pod-execution-role-trust-policy.json pod-execution-role-trust-policy_new.json
@@ -80,31 +70,29 @@ if [[ $fargate_pod_execution_role_arn != arn* ]]; then
     aws iam attach-role-policy \
         --policy-arn arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy \
         --role-name AmazonEKSFargatePodExecutionRole --profile saml
-
 fi
 
-echo fargate_pod_execution_role_arn is $fargate_pod_execution_role_arn
+echo "fargate_pod_execution_role_arn is $fargate_pod_execution_role_arn"
 
 if [ "$Operation" = "delete" ]; then
     eksctl delete cluster -f cluster_new.yaml --profile saml
-    aws cloudformation delete-stack --stack-name $vpc_name --profile saml
-    sleep 120
+    aws cloudformation delete-stack --stack-name "$VPC_NAME" --profile saml
+    aws cloudformation wait stack-delete-complete --stack-name "$VPC_NAME" --profile saml
     exit 1
 else 
     aws cloudformation create-stack \
-    --stack-name $vpc_name \
+    --stack-name "$VPC_NAME" \
     --template-body file://"codebuild-vpc-cfn.yaml" \
-    --parameters ParameterKey=EnvironmentName,ParameterValue=$vpc_name \
+    --parameters ParameterKey=EnvironmentName,ParameterValue="$VPC_NAME" \
     --profile saml
+    aws cloudformation wait stack-create-complete --stack-name "$VPC_NAME" --profile saml
 
-    sleep 300
+    echo "$VPC_NAME creation completed"
 
-    echo "$vpc_name creation completed"
-
-    vpc_id=$(aws cloudformation describe-stacks --query "Stacks[?StackName=='$vpc_name'][].Outputs[?OutputKey=='VPC'].OutputValue" --region $REGION --output text --profile saml)
-    sg_id=$(aws cloudformation describe-stacks --query "Stacks[?StackName=='$vpc_name'][].Outputs[?OutputKey=='NoIngressSecurityGroup'].OutputValue" --region $REGION --output text --profile saml)
-    private_subnet1=$(aws cloudformation describe-stacks --query "Stacks[?StackName=='$vpc_name'][].Outputs[?OutputKey=='PrivateSubnet1'].OutputValue" --region $REGION --output text --profile saml)
-    private_subnet2=$(aws cloudformation describe-stacks --query "Stacks[?StackName=='$vpc_name'][].Outputs[?OutputKey=='PrivateSubnet2'].OutputValue" --region $REGION --output text --profile saml)
+    vpc_id=$(aws cloudformation describe-stacks --query "Stacks[?StackName=='$VPC_NAME'][].Outputs[?OutputKey=='VPC'].OutputValue" --region "$REGION" --output text --profile saml)
+    sg_id=$(aws cloudformation describe-stacks --query "Stacks[?StackName=='$VPC_NAME'][].Outputs[?OutputKey=='NoIngressSecurityGroup'].OutputValue" --region "$REGION" --output text --profile saml)
+    private_subnet1=$(aws cloudformation describe-stacks --query "Stacks[?StackName=='$VPC_NAME'][].Outputs[?OutputKey=='PrivateSubnet1'].OutputValue" --region "$REGION" --output text --profile saml)
+    private_subnet2=$(aws cloudformation describe-stacks --query "Stacks[?StackName=='$VPC_NAME'][].Outputs[?OutputKey=='PrivateSubnet2'].OutputValue" --region "$REGION" --output text --profile saml)
 
     cp cluster.yaml cluster_new.yaml
 
@@ -116,7 +104,7 @@ else
     sed -i.bak -e "s|\$service_role_arn|$service_role_arn|" "cluster_new.yaml"
     sed -i.bak -e "s|\$REGION|$REGION|" "cluster_new.yaml"
     sed -i.bak -e "s|\$CLUSTER|$CLUSTER|" "cluster_new.yaml"
-    sed -i.bak -e "s|\$BRIDGE_DEPLOYMENT_POLICY_ARN|$BRIDGE_DEPLOYMENT_POLICY_ARN|" "cluster_new.yaml"
+    sed -i.bak -e "s|\$SECRET_ARN|$SECRET_ARN|" "cluster_new.yaml"
 
     #########################
     # eks cluster creation script
@@ -126,7 +114,7 @@ else
 
     echo "EKS cluster creation completed"
 
-    aws eks update-kubeconfig --name $CLUSTER --region $REGION --profile saml
+    aws eks update-kubeconfig --name "$CLUSTER" --region "$REGION" --profile saml
 
     helm repo add external-secrets https://charts.external-secrets.io
 
@@ -137,9 +125,10 @@ else
     -n external-secrets \
     --create-namespace \
     --set installCRDs=true \
-    --set webhook.port=9443 
+    --set webhook.port=9443 \
+    --wait
 
-    sleep 200
+    sleep 50
 
     echo "external-secrets installed"
     cp externalsecretstore.yaml externalsecretstore_new.yaml
@@ -148,8 +137,10 @@ else
     kubectl apply -f externalsecretstore_new.yaml
 
     sleep 50
+    cp externalsecret.yaml externalsecret_new.yaml
+    sed -i.bak -e "s|\$SECRET_ID|$SECRET_ID|" "externalsecret_new.yaml"
 
-    kubectl apply -f externalsecret.yaml
+    kubectl apply -f externalsecret_new.yaml
 
     sleep 50
 
